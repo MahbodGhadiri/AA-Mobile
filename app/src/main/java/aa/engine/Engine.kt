44f29736c22c6da74b45675a866e89c209c366c5
Mahbod Ghadiri Sani <m.ghadirisani2013@gmail.com>
2024-03-11 19:28:11 +0330

    putting collision job in the game loop

--- app/src/main/java/aa/engine/Engine.kt
+++ app/src/main/java/aa/engine/Engine.kt
@@ -4,6 +4,8 @@ import aa.engine.elements.MainCircle
 import aa.engine.elements.SmallBall
 import aa.engine.helpers.ExecutionContext
 import aa.engine.jobs.JobManager
+import aa.engine.jobs.critical.CollisionJob
+//import aa.engine.jobs.critical.MoveUpJob
 import aa.engine.jobs.critical.OrbitDetectionJob
 import aa.engine.jobs.critical.OrbitingJob
 import aa.engine.jobs.critical.SpawnJob
@@ -22,6 +24,7 @@ class Engine(mainCircle: MainCircle, smallBalls: ArrayList<SmallBall>) {
 //        jobManager.addJobToMovingPeriod(MoveUpJob())
         jobManager.addJobToDetectionPeriod(OrbitDetectionJob())
         jobManager.addJobToTweakingPeriod(SpawnJob());
+        jobManager.addJobToDetectionPeriod(CollisionJob())
     }
 
     public fun play(callback: () -> Unit): Timer {
