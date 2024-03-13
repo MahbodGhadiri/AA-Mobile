0ac943e3ac6fa6cb3d85cca7d47c733c7a956c81
joseph.o <josephosan1381@gmail.com>
2024-03-14 01:19:56 +0330

    added game activity, with elements

--- app/src/main/java/aa/android/activities/GameOverActivity.kt
+++ app/src/main/java/aa/android/activities/GameOverActivity.kt
@@ -1,16 +1,29 @@
 package aa.android.activities
 
+import aa.android.R
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class GameOverActivity : AppCompatActivity() {
     override fun onCreate(
-        savedInstanceState: Bundle?,
-        persistentState: PersistableBundle?
+        savedInstanceState: Bundle?
     ) {
         enableEdgeToEdge();
-        super.onCreate(savedInstanceState, persistentState)
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gameover);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars =
+                insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(
+                systemBars.left,
+                systemBars.top,
+                systemBars.right,
+                systemBars.bottom
+            )
+            insets
+        }
     }
 }
