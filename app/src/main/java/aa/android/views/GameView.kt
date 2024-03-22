9c5f4ae99773065b785957eadcb6938c6b92955c
joseph.o <josephosan1381@gmail.com>
2024-03-22 15:11:40 +0330

    fixed the problem with fast shooting

--- app/src/main/java/aa/android/views/GameView.kt
+++ app/src/main/java/aa/android/views/GameView.kt
@@ -145,8 +145,7 @@ public class GameView(context: Context, attrs: AttributeSet) :
     private fun generateSmallBalls(level: Level): ArrayList<SmallBall> {
         smallBalls.removeAll(smallBalls.toSet());
         for (i in 1..level.getHiddenBallNum()) {
-            smallBalls.add(AndroidSmallBall())
-        }
+            smallBalls.add(AndroidSmallBall()); }
         smallBalls.add(AndroidSmallBall(SmallBallStatus.SPAWNING))
         for (theta in level.getSpinningBallsTheta()) {
             smallBalls.add(AndroidSmallBall(SmallBallStatus.SPINNING, theta));
